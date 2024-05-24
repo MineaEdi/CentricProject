d0fe637db0aeaa2035ca10d66b24cc9299290efc
TudorCMarian <tudor-constantin.marian@student.tuiasi.ro>
2024-05-24 21:10:13 +0300

    Added sorting tests to be fixed ZtoA

---
+++ CentricAutomationTesting/PageObjectModel/ProductPage.cs
@@ -0,0 +1,43 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CentricAutomationTesting.PageObjectModel
+{
+	public class ProductPage
+	{
+		IWebDriver driver;
+
+		public ProductPage(IWebDriver browser)
+		{
+			driver = browser;
+		}
+
+		public IWebElement SortDropdown => driver.FindElement(By.XPath("//*[@id=\"header_container\"]/div[2]/div/span/select"));
+
+		/// <summary>
+		/// Variables for the price sorting
+		/// </summary>
+		public IWebElement FirstProductPrice => driver.FindElement(By.XPath("/html/body/div/div/div/div[2]/div/div/div/div[1]/div[2]/div[2]/div"));
+		public IWebElement SecondProductPrice => driver.FindElement(By.XPath("/html/body/div/div/div/div[2]/div/div/div/div[2]/div[2]/div[2]/div"));
+
+		/// <summary>
+		/// Variables for the name sorting
+		/// </summary>
+
+		public IWebElement FirstProductName => driver.FindElement(By.XPath("//*[@id=\"item_4_title_link\"]/div"));
+		public IWebElement SecondProductName => driver.FindElement(By.XPath("//*[@id=\"item_0_title_link\"]/div"));
+
+		public void SortProducts(string sortOrder)
+		{
+			var sortSelect = new SelectElement(SortDropdown);
+			sortSelect.SelectByText(sortOrder);
+			Thread.Sleep(2000); // Wait for sorting to complete
+		}
+	}
+}
--- CentricAutomationTesting/SauceDemoTest.cs
+++ CentricAutomationTesting/SauceDemoTest.cs
@@ -8,74 +8,155 @@ using System.Threading;
 
 namespace CentricAutomationTesting
 {
-    [TestClass]
-    public class SauceDemoTest
-    {
-        private ChromeDriver driver = new ChromeDriver();
+	[TestClass]
+	public class SauceDemoTest
+	{
+		private ChromeDriver driver = new ChromeDriver();
 
-        private HomePage homePage;
-        private LoginPage loginPage;
-        private LogoutPage logoutPage;
-        private OrderList order;
+		private HomePage homePage;
+		private LoginPage loginPage;
+		private LogoutPage logoutPage;
+		private OrderList order;
+		private ProductPage productPage;
 
-        [TestInitialize]
-        public void TestInitialize()
-        {
-            //access the browser
-            ChromeOptions options = new ChromeOptions();
-            options.AddArgument("--incognito");
+		[TestInitialize]
+		public void TestInitialize()
+		{
+			//access the browser
+			ChromeOptions options = new ChromeOptions();
+			options.AddArgument("--incognito");
 
-            driver.Manage().Window.Maximize();
-            driver.Navigate().GoToUrl("https://saucedemo.com/");
-        }
+			driver.Manage().Window.Maximize();
+			driver.Navigate().GoToUrl("https://saucedemo.com/");
+		}
 
-        [TestMethod]
-        public void LogInAccount()
-        {
-            loginPage = new LoginPage(driver);
-            Thread.Sleep(2000);
-            loginPage.LogInToAccount("standard_user", "secret_sauce");
-            Thread.Sleep(2000);
+		[TestMethod]
+		public void LogInAccount()
+		{
+			loginPage = new LoginPage(driver);
+			Thread.Sleep(2000);
+			loginPage.LogInToAccount("standard_user", "secret_sauce");
+			Thread.Sleep(2000);
 
-            homePage = new HomePage(driver);
+			homePage = new HomePage(driver);
 
-            Assert.IsTrue(homePage.GetWelcomeText().Contains("Products"));
-        }
+			Assert.IsTrue(homePage.GetWelcomeText().Contains("Products"));
+		}
 
-        [TestMethod]
-        public void LogOutAccount() 
-        {
-            LogInAccount();
-            logoutPage = new LogoutPage(driver);
-            logoutPage.LogOutOffAccount();
+		[TestMethod]
+		public void LogOutAccount()
+		{
+			LogInAccount();
+			logoutPage = new LogoutPage(driver);
+			logoutPage.LogOutOffAccount();
 
-            homePage = new HomePage(driver);
+			homePage = new HomePage(driver);
 
-            Assert.IsTrue(homePage.GetMainPageText().Contains("Accepted usernames are:"));
-        }
+			Assert.IsTrue(homePage.GetMainPageText().Contains("Accepted usernames are:"));
+		}
 
-        [TestMethod]
-        public void LogInFullOrder()
-        {
-            LogInAccount();
-            order = new OrderList(driver);
+		[TestMethod]
+		public void LogInFullOrder()
+		{
+			LogInAccount();
+			order = new OrderList(driver);
 
-            order.OrderCart();
+			order.OrderCart();
 
-            order.OrderCheckOut();
+			order.OrderCheckOut();
 
-            order.FillInfo("Anton", "Beton", "123456");
+			order.FillInfo("Anton", "Beton", "123456");
 
-            homePage = new HomePage(driver);
+			homePage = new HomePage(driver);
 
-            Assert.IsTrue(homePage.GetOrderFinishText().Contains("Thank you for your order!"));
-        }
+			Assert.IsTrue(homePage.GetOrderFinishText().Contains("Thank you for your order!"));
+		}
 
-        [TestCleanup]
-        public void TestCleanup()
-        {
-            Thread.Sleep(3000);
-            driver.Quit();
-        }
-    }
+		[TestMethod]
+		public void SortProductsAscDesc()
+		{
+			LogInAccount();
+
+			productPage = new ProductPage(driver);
+
+			productPage.SortDropdown.Click();
+			productPage.SortProducts("Price (low to high)");
+
+			double firstPrice = Convert.ToDouble(productPage.FirstProductPrice.Text.Replace("$", ""));
+			double secondPrice = Convert.ToDouble(productPage.SecondProductPrice.Text.Replace("$", ""));
+
+			Thread.Sleep(3000);
+
+
+			Assert.IsTrue(firstPrice < secondPrice);
+		}
+
+		[TestMethod]
+		public void SortProductsDescAsc()
+		{
+			LogInAccount();
+
+			productPage = new ProductPage(driver);
+
+			productPage.SortDropdown.Click();
+			productPage.SortProducts("Price (high to low)");
+
+			double firstPrice = Convert.ToDouble(productPage.FirstProductPrice.Text.Replace("$", ""));
+			double secondPrice = Convert.ToDouble(productPage.SecondProductPrice.Text.Replace("$", ""));
+
+			Thread.Sleep(3000);
+
+
+			Assert.IsTrue(firstPrice > secondPrice);
+		}
+
+
+		[TestMethod]
+		public void TestSortProductsAToZ()
+		{
+			LogInAccount();
+
+			productPage = new ProductPage(driver);
+
+			productPage.SortDropdown.Click();
+			productPage.SortProducts("Name (A to Z)");
+
+			string firstProductName = productPage.FirstProductName.Text;
+			string secondProductName = productPage.SecondProductName.Text;
+
+			Thread.Sleep(3000);
+
+			Assert.IsTrue(string.Compare(firstProductName, secondProductName) < 0);
+		}
+
+
+		/// <summary>
+		/// To be verified: The Assert is failing, but the method works fine
+		/// </summary>
+		[TestMethod]
+		public void TestSortProductsZToA()
+		{
+			LogInAccount();
+			productPage = new ProductPage(driver);
+
+			productPage.SortDropdown.Click();
+			productPage.SortProducts("Name (Z to A)");
+
+			string firstProductName = productPage.FirstProductName.Text;
+			string secondProductName = productPage.SecondProductName.Text;
+
+			Thread.Sleep(3000);
+
+			Assert.IsTrue(firstProductName.CompareTo(secondProductName) > 0);
+
+			//Assert.IsTrue(string.Compare(firstProductName, secondProductName) > 0);
+		}
+
+		[TestCleanup]
+		public void TestCleanup()
+		{
+			Thread.Sleep(3000);
+			driver.Quit();
+		}
+	}
 }
